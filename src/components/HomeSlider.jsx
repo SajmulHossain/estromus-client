8e6d41a5f690ed34b577601b1482935556f0ac0c
Sajmul <sajmul1427@gmail.com>
2024-12-08 20:43:43 +0600

    new slider added

--- src/components/HomeSlider.jsx
+++ src/components/HomeSlider.jsx
@@ -7,7 +7,9 @@ import "swiper/css/navigation";
 
 import slider1 from '../assets/slider1.jpg'
 import slider2 from '../assets/slider2.jpg'
-import slider3 from '../assets/slider3.jpg'
+import avengerImg from '../assets/avenger.jpg'
+import matrixImg from '../assets/matrix.jpg'
+import spiderMan from '../assets/spiderMan.jpg'
 
 
 const HomeSlider = () => {
@@ -33,7 +35,13 @@ const HomeSlider = () => {
         <img src={slider2} className="object-cover w-full h-[400px]" alt="slider2" />
       </SwiperSlide>
       <SwiperSlide>
-        <img src={slider3} className="object-cover w-full h-[400px]" alt="slider3" />
+        <img src={spiderMan} className="object-cover w-full h-[400px]" alt="slider3" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={avengerImg} className="object-cover w-full h-[400px]" alt="slider3" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={matrixImg} className="object-cover w-full h-[400px]" alt="slider3" />
       </SwiperSlide>
     </Swiper>
   );
